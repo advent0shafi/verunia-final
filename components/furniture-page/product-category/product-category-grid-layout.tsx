d8f6f485850d4d61b74fe614c5725769f2f774f9
advent0shafi <shafimsm19@gmail.com>
2026-02-10 09:36:12 +0530

    added the images

--- components/furniture-page/product-category/product-category-grid-layout.tsx
+++ components/furniture-page/product-category/product-category-grid-layout.tsx
@@ -2,18 +2,25 @@ import ProductCategoryGrid from "./product-catogery-grid";
 import SidebarFilter from "./sidebar-filter";
 
 export default function ProductCategoryGridLayout() {
-    return (
-        <section className="bg-white py-12 md:py-16 lg:py-20">
-            <div className="max-w-[1440px] mx-auto px-6 md:px-12 lg:px-16">
+  return (
+    <section className="bg-white py-10 sm:py-12 md:py-16 lg:py-20">
+      <div className="max-w-[1440px] mx-auto px-4 sm:px-6 md:px-12 lg:px-16">
 
+        {/* Layout wrapper */}
+        <div className="flex flex-col lg:flex-row gap-8 lg:gap-16">
+          
+          {/* Sidebar */}
+          <aside className="w-full lg:w-[260px]">
+            <SidebarFilter />
+          </aside>
 
-                <div className="flex gap-16">
-                    <SidebarFilter />
-                    <section className="flex-1">
-                        <ProductCategoryGrid />
-                    </section>
-                </div>
-            </div>
-        </section>
-    );
+          {/* Product Grid */}
+          <section className="flex-1">
+            <ProductCategoryGrid />
+          </section>
+
+        </div>
+      </div>
+    </section>
+  );
 }
