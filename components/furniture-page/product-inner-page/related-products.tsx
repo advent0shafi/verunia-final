e4ca055d1a186511320d14c4238072ea8c53d86b
advent0shafi <shafimsm19@gmail.com>
2026-02-11 11:05:38 +0530

    added the

--- components/furniture-page/product-inner-page/related-products.tsx
+++ components/furniture-page/product-inner-page/related-products.tsx
@@ -32,7 +32,7 @@ export default function RelatedProducts({ products }: RelatedProductsProps) {
             View All Chairs
           </Link>
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-6">
           {products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
